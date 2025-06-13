4d8f2a53487c41c3b92e7811a3105ff11b241e4d
huffyyy <husnulf936@gmail.com>
2025-06-13 21:20:58 +0700

    menambahkan fitur shopping cart

--- js/script.js
+++ js/script.js
@@ -26,13 +26,13 @@ const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
 const sc = document.querySelector("#shopping-cart-button");
 document.addEventListener("click", function (e) {
-  if (!hm.contains(e.target) && !navbarNav.contains(e.taget)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
-  if (!sb.contains(e.target) && !searchForm.contains(e.taget)) {
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
   }
-  if (!sc.contains(e.target) && !shoppingCart.contains(e.taget)) {
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
     shoppingCart.classList.remove("active");
   }
 });
